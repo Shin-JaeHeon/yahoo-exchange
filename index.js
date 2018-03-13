eb0202eaebab1bc143226cee49c107ebd07ddc27
Shin-JaeHeon <heoni0504@gmail.com>
2018-03-13 09:29:46 +0900

    1.0.0-alpha.3 Pre-release

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const request = require("request");
+const parseHTML = html => parseFloat(html.toString().split('react-text: 36 -->')[1].split('<')[0].replace(',', ''));
 function getData(pair, callback, errorHandler = err => console.log(err)) {
     request({
         url: `https://finance.yahoo.com/quote/${pair}=X?p=${pair}=X`,
@@ -9,19 +10,19 @@ function getData(pair, callback, errorHandler = err => console.log(err)) {
         if (err)
             errorHandler(err, pair);
         else
-            callback(parseFloat(html.toString().split('react-text: 36 -->')[1].split('<')[0].replace(',', '')), pair);
+            callback(parseHTML(html), pair);
     });
 }
 exports.getData = getData;
 function getDataArray(pair, callback, errorHandler = err => console.log(err)) {
-    pair.map(v => request({
+    pair.forEach(v => request({
         url: `https://finance.yahoo.com/quote/${v}=X?p=${v}=X`,
         encoding: null,
     }, (err, response, html) => {
         if (err)
             errorHandler(err, v);
         else
-            callback(parseFloat(html.toString().split('react-text: 36 -->')[1].split('<')[0].replace(',', '')), v);
+            callback(parseHTML(html), v);
     }));
 }
 exports.getDataArray = getDataArray;
--- index.ts
+++ index.ts
@@ -1,22 +1,24 @@
 import request = require("request");
 
+const parseHTML = html => parseFloat(html.toString().split('react-text: 36 -->')[1].split('<')[0].replace(',', ''));
+
 export function getData(pair: string, callback: (data: number, pair?: string) => any, errorHandler: (error: Error, pair?: String) => any = err => console.log(err)): void {
     request({
         url: `https://finance.yahoo.com/quote/${pair}=X?p=${pair}=X`,
         encoding: null,
     }, (err, response, html) => {
         if (err) errorHandler(err, pair);
-        else callback(parseFloat(html.toString().split('react-text: 36 -->')[1].split('<')[0].replace(',', '')), pair);
+        else callback(parseHTML(html), pair);
     });
 }
 
 export function getDataArray(pair: Array<string>, callback: (data: number, pair?: string) => any, errorHandler: (error: Error, pair?: String) => any = err => console.log(err)): void {
-    pair.map(v => request({
+    pair.forEach(v => request({
         url: `https://finance.yahoo.com/quote/${v}=X?p=${v}=X`,
         encoding: null,
     }, (err, response, html) => {
         if (err) errorHandler(err, v);
-        else callback(parseFloat(html.toString().split('react-text: 36 -->')[1].split('<')[0].replace(',', '')), v);
+        else callback(parseHTML(html), v);
     }));
 }
 
