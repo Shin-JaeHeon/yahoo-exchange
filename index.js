684b29754df0e9cfba02313c9cf0da2798bb7eb2
Shin-JaeHeon <heoni0504@gmail.com>
2018-03-20 23:05:47 +0900

    1.1.0 Release, Now you can get more data. (Previous Close, Open, Bid, Ask)

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const request = require("request");
-const parseHTML = html => parseFloat(html.toString().split('react-text: 36 -->')[1].split('<')[0].replace(',', ''));
-const parseHTML2 = h => {
-    const html = h.toString();
-    return [parseFloat(html.split('react-text: 36 -->')[1].split('<')[0].replace(',', '')),
-        parseFloat(html.split('react-text: 38 -->')[2].split('<')[0] + html.split('react-text: 39 -->')[1].split(' ')[0]),
-        parseFloat(html.split('react-text: 39 -->')[1].split('(')[1].split("\%")[0])
-    ];
-};
+const nonComma = (str) => str.replace(',', '');
+const parseReact = (str, number, num) => str.split(`react-text: ${number} -->`)[num];
+const parseR = (str) => number => parseFloat(nonComma(parseReact(str, number, 1).split('<')[0]));
+const parseHTML = html => parseR(html.toString())(36);
+const parseHTML2 = (html, r) => [r(36), html.indexOf('react-text: 39 -->') === -1 ? parseFloat(parseReact(html, 38, 2).split(' ')[0]) : parseFloat(parseReact(html, 39, 1).split(' ')[0]),
+    html.indexOf('react-text: 39 -->') === -1 ? parseFloat(parseReact(html, 38, 2).split('(')[1].split("\%")[0]) : parseFloat(parseReact(html, 39, 1).split('(')[1].split("\%")[0]), r(42), r(48), r(54), r(71),
+];
+const req = (pair, errorHandler, callback) => request({
+    url: `https://finance.yahoo.com/quote/${pair}=X?p=${pair}=X`,
+    encoding: null,
+}, (err, response, html) => {
+    if (err)
+        errorHandler(err, pair);
+    else
+        callback(parseHTML2(html.toString().replace(',', ''), parseR(html.toString())), pair);
+});
 /**
  * @deprecated Since version 1.0. Will be deleted in version 2.0. Use getExchangeDataArray instead.
  */
@@ -40,28 +48,10 @@ function getDataArray(pair, callback, errorHandler = err => console.log(err)) {
 }
 exports.getDataArray = getDataArray;
 function getExchangeDataArray(pair, callback, errorHandler = err => console.log(err)) {
-    if (typeof pair === 'string') {
-        request({
-            url: `https://finance.yahoo.com/quote/${pair}=X?p=${pair}=X`,
-            encoding: null,
-        }, (err, response, html) => {
-            if (err)
-                errorHandler(err, pair);
-            else
-                callback(parseHTML2(html), pair);
-        });
-    }
-    else if (Array.isArray(pair)) {
-        pair.forEach(v => request({
-            url: `https://finance.yahoo.com/quote/${v}=X?p=${v}=X`,
-            encoding: null,
-        }, (err, response, html) => {
-            if (err)
-                errorHandler(err, v);
-            else
-                callback(parseHTML2(html), v);
-        }));
-    }
+    if (typeof pair === 'string')
+        req(pair, errorHandler, callback);
+    else if (Array.isArray(pair))
+        pair.forEach(v => req(v, errorHandler, callback));
 }
 exports.getExchangeDataArray = getExchangeDataArray;
 function getPairArray(currency, base) {
--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 import request = require("request");
 
-const parseHTML = html => parseFloat(html.toString().split('react-text: 36 -->')[1].split('<')[0].replace(',', ''));
-
-const parseHTML2 = h => {
-    const html = h.toString();
-    return [parseFloat(html.split('react-text: 36 -->')[1].split('<')[0].replace(',', '')),
-        parseFloat(html.split('react-text: 38 -->')[2].split('<')[0] + html.split('react-text: 39 -->')[1].split(' ')[0]),
-        parseFloat(html.split('react-text: 39 -->')[1].split('(')[1].split("\%")[0])
+const nonComma = (str: string) => str.replace(',', '');
+const parseReact = (str: string, number, num: number) => str.split(`react-text: ${number} -->`)[num];
+const parseR = (str: string) => number => parseFloat(nonComma(parseReact(str, number, 1).split('<')[0]));
+const parseHTML = html => parseR(html.toString())(36);
+const parseHTML2 = (html: string, r: Function) => [r(36), html.indexOf('react-text: 39 -->') === -1 ? parseFloat(parseReact(html, 38, 2).split(' ')[0]) : parseFloat(parseReact(html, 39, 1).split(' ')[0]),
+    html.indexOf('react-text: 39 -->') === -1 ? parseFloat(parseReact(html, 38, 2).split('(')[1].split("\%")[0]) : parseFloat(parseReact(html, 39, 1).split('(')[1].split("\%")[0]), r(42), r(48), r(54), r(71),
+    /* Day's Range / 52 Week Range
+     * parseFloat(html.split('data-reactid="61">')[2].split(' ')[0]), parseFloat(html.split('data-reactid="61">')[2].split(' ')[2].split('<')[0]),
+     * parseFloat(html.split('data-reactid="65">')[2].split(' ')[0]), parseFloat(html.split('data-reactid="65">')[2].split(' ')[2].split('<')[0])
+     */
     ];
-};
+const req = (pair, errorHandler, callback) => request({
+    url: `https://finance.yahoo.com/quote/${pair}=X?p=${pair}=X`,
+    encoding: null,
+}, (err, response, html) => {
+    if (err) errorHandler(err, pair);
+    else callback(parseHTML2(html.toString().replace(',', ''), parseR(html.toString())), pair);
+});
 
 /**
  * @deprecated Since version 1.0. Will be deleted in version 2.0. Use getExchangeDataArray instead.
@@ -38,24 +47,8 @@ export function getDataArray(pair: Array<string>, callback: (data: any, pair?: s
 }
 
 export function getExchangeDataArray(pair: any, callback: (data: Array<number>, pair?: string) => any, errorHandler: (error: Error, pair?: String) => any = err => console.log(err)): void {
-    if (typeof pair === 'string') {
-        request({
-            url: `https://finance.yahoo.com/quote/${pair}=X?p=${pair}=X`,
-            encoding: null,
-        }, (err, response, html) => {
-            if (err) errorHandler(err, pair);
-            else callback(parseHTML2(html), pair);
-
-        });
-    } else if (Array.isArray(pair)) {
-        pair.forEach(v => request({
-            url: `https://finance.yahoo.com/quote/${v}=X?p=${v}=X`,
-            encoding: null,
-        }, (err, response, html) => {
-            if (err) errorHandler(err, v);
-            else callback(parseHTML2(html), v);
-        }));
-    }
+    if (typeof pair === 'string') req(pair, errorHandler, callback);
+    else if (Array.isArray(pair)) pair.forEach(v => req(v, errorHandler, callback));
 }
 
 export function getPairArray(currency: Array<string>, base: Array<string>): Array<string> {
